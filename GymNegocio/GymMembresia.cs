1b89ce5c06d0c301e69abd8722da0c02d8c6f9e8
rachelitab2 <madeg@RECLUTAMIENTORH>
2025-06-05 04:54:55 +0200

    ver si se agrega los cambios

--- GymNegocio/GymMembresia.cs
+++ GymNegocio/GymMembresia.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Data.SqlClient; 
 using GymDatos;
-using Microsoft.Data.SqlClient; 
+using Microsoft.Data.SqlClient; // verificacion de push
 
 namespace GymNegocio
 //el nombre es importante para la llamada correcta y demas...
