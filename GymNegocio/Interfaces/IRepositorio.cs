f089d3d53be3b88bc070bec932065579c0f355d6
rachelitab2 <madeg@RECLUTAMIENTORH>
2025-08-06 21:37:27 -0400

    intento de implementacion de Interfaz

---
+++ GymNegocio/Interfaces/IRepositorio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymNegocio.Interfaces
+{
+        public interface IRepositorio<T>
+        {
+            Task<List<T>> ListarAsync();
+            Task InsertarAsync(T entidad);
+            Task ActualizarAsync(T entidad);
+            Task EliminarAsync(int id);
+        }
+    
+}
+/*
+ * Interfaz genérica para el acceso a datos en el sistema de gimnasio.
+ * Define las operaciones CRUD básicas (Crear, Leer, Actualizar, Eliminar) de forma asíncrona.
+ * Todas las clases de repositorio (Membresía, Cliente, Entrenador, etc.) pueden implementar esta interfaz
+ * para garantizar un acceso a datos consistente y reutilizable.
+ * 
+ * Requisito técnico solicitado por el maestro: uso de interfaces y métodos asíncronos.
+ * 
+ * Autor: [Tu nombre o grupo]
+ * Fecha: [Fecha de implementación]
+ * 
+ * TODO: Implementar esta interfaz en las clases de acceso a datos para cada entidad.
+ */
---
+++ GymNegocio/PermisosUsuario/ServicioPermisos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using GymDatos;
+using Microsoft.Data.SqlClient;
+
+namespace GymNegocio.Login
+{
+    public class ServicioPermisos
+    {
+        private readonly string _conexion = ConexionDatos.Conexion;
+
+        public List<string> ObtenerPermisosPorRol(string rol)
+        {
+            List<string> permisos = new List<string>();
+
+            using (SqlConnection conn = new SqlConnection(_conexion))
+            {
+                conn.Open();
+                string query = @"
+                    SELECT p.Nombre
+                    FROM Permisos p
+                    INNER JOIN RolPermisos rp ON p.Id = rp.PermisoId
+                    WHERE rp.Rol = @Rol";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@Rol", rol);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            permisos.Add(reader.GetString(0));
+                        }
+                    }
+                }
+            }
+
+            return permisos;
+        }
+
+        public bool TienePermiso(string rol, string permiso)
+        {
+            var permisos = ObtenerPermisosPorRol(rol);
+            return permisos.Contains(permiso);
+        }
+    }
+}
